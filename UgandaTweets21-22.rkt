7e56ec5bacb8bffef7f1d3bf0fd709e7d8d859aa
Mugisha Ivan Jalagatha <mugishaivanj@gmail.com>
2024-12-08 22:51:50 +0300

    add end of semester project

---
+++ UgandaTweets21-22.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(require net/url)
+(require data-science-master)
+(require plot)
+(require math)
+
+
+(define data (read-csv "C:/Users/MUGIS/Desktop/end_of_sem/uganda.csv" #:->number? #f #:header? #t))
+
+(define sample_data
+  (let ([tmp (map (λ (x) (list (list-ref x 9))) data)])
+    (filter (λ (x) (not (string-prefix? (first x) "RT"))) tmp)))
+(define f_sample_data (flatten sample_data))
+(define all_tweet-text (apply string-append f_sample_data))
+
+(define p_tweet-text (string-normalize-spaces
+                      (remove-punctuation
+                       (string-downcase all_tweet-text) #:websafe? #t)))
+
+(define words (document->tokens p_tweet-text #:sort? #t))
+
+
+(define sentiment (list->sentiment words #:lexicon 'nrc))
+
+ 
+(aggregate sum ($ sentiment 'sentiment) ($ sentiment 'freq))
+
+(let ([counts (aggregate sum ($ sentiment 'sentiment) ($ sentiment 'freq))])
+  (parameterize ((plot-width 800))
+    (plot (list
+	   (tick-grid)
+	   (discrete-histogram
+	    (sort counts (λ (x y) (> (second x) (second y))))
+	    #:color "MediumSlateBlue"
+	    #:line-color "MediumSlateBlue"))
+	  #:x-label "Affective Label"
+	  #:y-label "Frequency")))
+
+(define sentiment_bing (list->sentiment words #:lexicon 'bing))
+(parameterize ([plot-height 200])
+  (plot (discrete-histogram
+	 (aggregate sum ($ sentiment_bing 'sentiment) ($ sentiment_bing 'freq))
+	 #:y-min 0
+	 #:y-max 8000
+	 #:invert? #t
+	 #:color "MediumOrchid"
+	 #:line-color "MediumOrchid")
+	#:x-label "Frequency"
+	#:y-label "Sentiment Polarity"))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# End of semester Project
+Using Scheme for sentimental analysis of Uganda Tweets The abstractions have also been used for analysis Twitter data see 
+Link to tweets :  http://www.nicholasvanhorn.com/posts/trump-tweets.html. For this project we built a set of abstractions that can be used by developers to build software systems that analyze the moods of tweets for a given country (e.g., Uganda) over a periodof 12 months. We assume that locality information provided by Twitter API is accurate.
+
+![This is the File Image](./images/uganda2.jpg)
